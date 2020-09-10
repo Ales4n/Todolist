ff0bb7023e1488f9c35b185fdaa43ee1e3f957c5
Alessandro Sansossio <san07ar@gmail.com>
2020-09-10 13:03:55 +0200

    some items added

--- src/index.js
+++ src/index.js
@@ -1,34 +1,27 @@
 const express = require('express')
 const ejs = require('ejs')
 const morgan = require('morgan')
+const path = require('path')
 const session = require('cookie-session');
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const todoController = require('./controllers/todoController')
+//const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const app = express()
 
 //Settings
 app.set('port', 3000)
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 //Using sessions
 app.use(session({secret: 'todotopsecret'}))
 
-//Set up template engine
- app.set('view engine', 'ejs')
-
 //static files
 app.use(express.static(path.join(__dirname, 'public')))
-//Routes
-.get('/todo', (req, res) => {
-        
-})
 
-.post('/todo/add', (req, res) => {
-     
-})
-
-.get('/todo/delete/:id', (req, res) => {
-     
-})
+//Routes/controlllers
+todoController(app)
 
 //Listening the server
 app.listen(app.get('port'), ()  => {
